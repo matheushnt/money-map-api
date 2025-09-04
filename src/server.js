1f8640fbb0d61dd3bf1d950cb5f60aee0561ddf4
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-03 22:05:10 -0300

    feat: adiciona rota de leitura de categorias

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
+import { randomUUID } from 'node:crypto';
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
-  res.end('ok');
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/categorias') {
+    const categoria = {
+      id: randomUUID(),
+      nome: 'investimenos',
+    };
+
+    return res.setHeader('Content-Type', 'application/json').end(JSON.stringify(categoria));
+  }
+
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
